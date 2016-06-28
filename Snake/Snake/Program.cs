3b86270463a1030acf573fee59875844a01067a6
Nikkitik <nikkitik21@gmail.com>
2016-06-28 18:30:16 +0300

    Кормление змейки

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -19,19 +19,34 @@ namespace Snake
             VerticalLine leftLine = new VerticalLine(0, 23, 0, '+');
             VerticalLine rightLine = new VerticalLine(0, 23, 78, '+');
 
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
             //Отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
-            snake.Draw();
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
  
             while(true)
             {
+                if(snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+
                 if(Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
