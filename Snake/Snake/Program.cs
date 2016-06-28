48e1c499b0a36b26849716556faeb3aebad95ef6
Nikkitik <nikkitik21@gmail.com>
2016-06-28 19:38:53 +0300

    Добавлено окончание игры

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -49,11 +49,29 @@ namespace Snake
                     snake.HandleKey(key.Key);
                 }
             }
+
+            WriteGameOver();
+            Console.ReadLine();
         }
 
-        static void Draw(Figure figure)
+        static void WriteGameOver()
         {
-            figure.Draw();
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("Автор: Романцов Никита", xOffset + 2, yOffset++);
+            WriteText("SPECIAL FOR YOU!!!", xOffset + 5, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
         }
     }
 }
